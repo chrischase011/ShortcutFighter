58576628b4b1f33a59cae086efe3130276081ed8
Christopher Robin Chase <christopherchase011@gmail.com>
2021-09-14 11:19:42 +0800

    ShortcutFighter

---
+++ ShortcutFighter.bat
@@ -0,0 +1,28 @@
+@echo off
+
+Rem This program recover files that has became shortcut files
+echo Please Run as Administrator
+echo.
+echo Welcome to Shortcut fighter where we will recover 
+echo.
+echo your files that has became a shortcut files
+echo.
+echo.
+echo First, identify what is the directory name of your USB Flash drive or Hard drive
+echo Example: D:/
+echo.
+:a
+set /p n="What directory drive?: "
+set path=n
+if [%n%]==[] (echo You should input your directory drive 
+goto a) else (
+attrib -h -r -s /s /d %n%*.*
+timeout /t 3 /NoBreak
+goto c)
+
+:c
+echo.
+echo.
+echo Check your files now
+echo.
+pause
